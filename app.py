43eb40f36bf97d38c2a332995e76292d5889dae3
Shushanth <shushanth03@gmail.com>
2025-06-17 08:10:27 +0530

    🔥 Clean version of backend with secret removed and submodule ignored

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+CORS(app, supports_credentials=True)
 
 # ===================== Init =====================
 app = Flask(__name__)
